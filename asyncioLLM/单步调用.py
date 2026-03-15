a68b097fbb62efdb256e1e1df1af1a04f79f6a77
suda-mtl <81677815+suda-mtl@users.noreply.github.com>
2026-03-15 21:34:32 +0800

    Update 单步调用.py

--- asyncioLLM/单步调用.py
+++ asyncioLLM/单步调用.py
@@ -2,7 +2,7 @@ import os
 from openai import OpenAI
 
 client = OpenAI(
-    api_key="sk-67b47bbff8134add99108d538629f886",
+    api_key="",
     base_url="https://api.deepseek.com")
 
 system_message="""
